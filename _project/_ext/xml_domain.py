c35eba9731ffb0d550256d3f492e964609841b6f
Vic Hargrave <vichargrave@gmail.com>
2015-11-02 11:46:11 -0800

    Project files to build ossec.github.io

---
+++ _project/_ext/xml_domain.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def setup(app):
+    app.add_object_type('element', 'xml', '%s')
+    app.add_object_type('attrubute', 'xml', 'pair: %s; Attrubute')
+    app.add_object_type('intopt', 'OS','%s')
+   
